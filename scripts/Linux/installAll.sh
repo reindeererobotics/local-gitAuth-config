a12cdd99fb76b39c532c91aa5d6db3c119897333
Joseph O <36710290+josephojo@users.noreply.github.com>
2022-12-31 16:26:55 -0500

    Bugfix: dotnet not found due to dep on sdk and env variable not set

--- scripts/Linux/installAll.sh
+++ scripts/Linux/installAll.sh
@@ -2,7 +2,11 @@
 
 # Install dotnet
 sudo chmod +x ./dotnet-install.sh
-./dotnet-install.sh --version latest --runtime aspnetcore
+./dotnet-install.sh # --version latest
+
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc
+source ~/.bashrc
 
 # Install GCM
 . installGCM.sh
